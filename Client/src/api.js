059a6527ae6d161d9884f6a4b8cb199dfc960f10
THOTA-BHANUPRAKASH <bhanuprakashthota9999@gmail.com>
2026-08-29 16:32:08 +0530

    Backend api updated in client

--- Client/src/api.js
+++ Client/src/api.js
@@ -1,2 +1,3 @@
-export const API_URL="http://localhost:3333"
+// export const API_URL="http://localhost:3333"
 export const VENDOR_URL="http://localhost:9999"
+export const API_URL="https://foodblast.onrender.com"
