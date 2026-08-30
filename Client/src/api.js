4d71df3811fc953daf0623708678b91682550194
THOTA-BHANUPRAKASH <bhanuprakashthota9999@gmail.com>
2026-08-30 22:45:28 +0530

    urls are updated

--- Client/src/api.js
+++ Client/src/api.js
@@ -1,3 +1,4 @@
 // export const API_URL="http://localhost:3333"
-export const VENDOR_URL="http://localhost:9999"
+//export const VENDOR_URL="http://localhost:9999"
 export const API_URL="https://foodblast.onrender.com"
+export const VENDOR_URL="https://food-blast-frontend.vercel.app/"
